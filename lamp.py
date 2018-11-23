740dc69658eb1faa1728fcc9181a08c176d1df05
Daniel Hobi <info@danielhobi.ch>
2018-11-23 12:33:20 +0100

    more print

--- lamp.py
+++ lamp.py
@@ -2,13 +2,23 @@ import RPi.GPIO as GPIO
 
 
 class Lamp:
-    def __init__(self, red = 16, green = 20, blue = 21, freq = 100):
+    def __init__(self):
+        print('lamp initialising...')
         GPIO.setmode(GPIO.BCM)
         GPIO.setwarnings(False)
+
+        red = 16
+        green = 20
+        blue = 21
+        freq = 100
 
         GPIO.setup(red, GPIO.OUT)
         GPIO.setup(green, GPIO.OUT)
         GPIO.setup(blue, GPIO.OUT)
+
+        GPIO.output(red, 1)
+        GPIO.output(green, 1)
+        GPIO.output(blue, 1)
 
         self.RED = GPIO.PWM(red, freq)
         self.GREEN = GPIO.PWM(green, freq)
@@ -17,6 +27,7 @@ class Lamp:
         self.RED.ChangeDutyCycle(100)
         self.GREEN.ChangeDutyCycle(100)
         self.RED.ChangeDutyCycle(100)
+        print('...done.')
 
     def __enter__(self):
         return self
